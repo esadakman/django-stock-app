39654e8252576baada3e6187fd128a2246b29137
esadakman <esadd26@gmail.com>
2022-10-06 15:06:53 +0300

    stock filter fin

--- stock/urls.py
+++ stock/urls.py
@@ -4,10 +4,10 @@ from .views import CategoryView, BrandView, ProductView, FirmView, StockView
 
 router = routers.DefaultRouter()
 # ! parantex içinde ilk yazılacak yer ile ikinci yere yalıcak yeri çalıştırıyoruz
-router.register('categories', CategoryView)
-router.register('brands', BrandView)
-router.register('product', ProductView)
-router.register('firm', FirmView)
+# router.register('categories', CategoryView)
+# router.register('brands', BrandView)
+# router.register('product', ProductView)
+# router.register('firm', FirmView)
 router.register('stock', StockView)
 
 
--- stock/views.py
+++ stock/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Brand, Category, Product, Firm,Stock
 from .serializers import CategorySerializer,  BrandSerializer, ProductSerializer, FirmSerializer, StockSerializer
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend 
+# * filtreleme işlemi yapmak için django-filtersı yükledik ve apps'e 'django_filters' ı ekledikten sonra viewsımızda import edip kullanıma hazır hale getirdik
 # Create your views here.
 
 
@@ -29,4 +30,8 @@ class ProductView(viewsets.ModelViewSet):
 class StockView(viewsets.ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    
+    # ! filtreleme yaparken iki ayrı filter kullanacağımız için iki filtrs'ımızıda yazıyoruz
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter] 
+    # ? ardından ise filterset_fields kısmında filterlamak istediğimiz parametreleri belirtiyoruz 
+    # ! product'tan sonra ''product__name'' yazarak product'ın name'ine ulaşabilirim
+    filterset_fields = ['product', 'firm', 'transaction'] 
