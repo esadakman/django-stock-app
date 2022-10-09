999a1b3c8844330a444a1e1224feb468b5088722
esadakman <esadd26@gmail.com>
2022-10-09 20:03:05 +0300

    CustomModelPermission

--- stock/permissions.py
+++ stock/permissions.py
@@ -1,1 +1,13 @@
-from rest_framework import permissions
+from rest_framework.permissions import DjangoModelPermissions
+
+# ! django model permission default olarak GET metodlarına bir permission belirlemiyor. Bu sebeple kendi 'GET' metodumuzu belirlemek için permissions'ı overwrite ediyoruz
+class CustomModelPermission(DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
--- stock/views.py
+++ stock/views.py
@@ -4,14 +4,16 @@ from .models import Brand, Category, Product, Firm, Transaction
 from .serializers import CategorySerializer,  BrandSerializer, ProductSerializer, FirmSerializer, TransactionSerializer, CategoryProductsSerializer
 # * filtreleme işlemi yapmak için django-filtersı yükledik ve apps'e 'django_filters' ı ekledikten sonra viewsımızda import edip kullanıma hazır hale getirdik
 from django_filters.rest_framework import DjangoFilterBackend
-# ! admin panelinden oluşturmuş olduğum kullanıcı rolü gruplarını kullanabilmek için DjangoModelPermissions'ı import ediyoruz
-from rest_framework.permissions import DjangoModelPermissions
+# ! admin panelinden oluşturmuş olduğum kullanıcı rolü gruplarını kullanabilmek için DjangoModelPermissions'ı import edip kullanabiliriz fakat kendi permiisions'ımızı yazdığımız için onu çağırıp permission_classes kısmına onu yazıyorum
+from rest_framework.permissions import DjangoModelPermissions 
+from .permissions import CustomModelPermission
+
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     # ? permissionları kullanabilmek için permission_class'ımı belirtiyorum
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomModelPermission]
     # ! filtreleme yaparken iki ayrı filter kullanacağımız için iki filtrs'ımızıda yazıyoruz
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     # ? ardından ise filterset_fields kısmında filterlamak istediğimiz parametreleri belirtiyoruz
@@ -30,7 +32,7 @@ class CategoryView(viewsets.ModelViewSet):
 class BrandView(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomModelPermission]
     # ! filtreleme için aynı işlemleri bu kısımda da yapıyoruz
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['name']
@@ -40,7 +42,7 @@ class BrandView(viewsets.ModelViewSet):
 class FirmView(viewsets.ModelViewSet):
     queryset = Firm.objects.all()
     serializer_class = FirmSerializer
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomModelPermission]
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
 
@@ -49,7 +51,7 @@ class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     fields = ["id", "name", "category",  "brand",  "stock"]
     serializer_class = ProductSerializer
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomModelPermission]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['category', 'brand']
     search_fields = ['name']
@@ -58,7 +60,7 @@ class ProductView(viewsets.ModelViewSet):
 class TransactionView(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [CustomModelPermission]
     # ! filtreleme yaparken iki ayrı filter kullanacağımız için iki filtrs'ımızıda yazıyoruz
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     # ? ardından ise filterset_fields kısmında filterlamak istediğimiz parametreleri belirtiyoruz
